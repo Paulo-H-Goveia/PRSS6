985939276f083969a2b29ea44a186a0ccdfe0534
paulo-h-goveia <goveia.paulo@aluno.ifsp.edu.br>
2023-11-10 10:12:23 -0300

    ajustes

--- GlicDiary/src/main/java/br/edu/ifsp/arq/prss6/glicdiary/service/MeasureService.java
+++ GlicDiary/src/main/java/br/edu/ifsp/arq/prss6/glicdiary/service/MeasureService.java
@@ -3,7 +3,9 @@ package br.edu.ifsp.arq.prss6.glicdiary.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifsp.arq.prss6.glicdiary.domain.model.Measure;
